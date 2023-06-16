c21c5c5c8eba2d91386ba53b242db561003ed4b0
Bel-kun <bellaire.vivien22@gmail.com>
2023-06-16 10:08:44 -0400

    Add header component

---
+++ front/components/organisms/header/header.php
@@ -0,0 +1,30 @@
+<header class="o-header">
+     
+     <div class="o-header__logo">
+       <img src="" alt="">
+     </div>
+     
+<!--      <div class="o-header__searchbar">
+       
+     </div>
+     
+     <nav>
+       
+       <a href="...">
+         
+       </a>
+       <a href="...">
+         
+       </a>
+     </nav> -->
+     
+     <div class="o-header__user">
+       <button>
+         Login
+       </button>
+       <button>
+         Sign up
+       </button>
+     </div>
+    
+  </header>
