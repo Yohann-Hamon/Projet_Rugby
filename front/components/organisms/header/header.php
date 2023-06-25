bd82d5a9626e0d18da2cc24866a5a05dff2b6c6a
Bel-kun <bellaire.vivien22@gmail.com>
2023-06-25 04:51:31 -0400

    Delete reset.css

--- front/components/organisms/header/header.php
+++ front/components/organisms/header/header.php
@@ -1,29 +1,38 @@
-<header class="o-header">
-     
-     <div class="o-header__logo">
-       <img src="" alt="">
-     </div>
-     
-<!--      <div class="o-header__searchbar">
-       
-     </div>
-     
-     <nav>
-       
-       <a href="...">
-         
-       </a>
-       <a href="...">
-         
-       </a>
-     </nav> -->
-     
-     <div class="o-header__user">
-       <button class="o-header__button">
-         Login
-       </button>
-       <button class="o-header__button">
-         Sign up
-       </button>
-     </div>
-</header>
+<!-- <link rel="stylesheet" href="./header.css"> -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <link rel="stylesheet" href="./front/components/molecules/buttons/button.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <header class="o-header">
+      
+      <div class="o-header__logo">
+        <img src="" alt="unstaged">
+      </div>
+      
+      <!-- <div class="o-header__user">
+        <button class="o-header__button">
+          Login
+        </button>
+        <button class="o-header__button">
+          Sign up
+        </button>
+      </div> -->
+
+      <?php
+          include './front/components/molecules/buttons/navbutton.php';
+          // include './front/components/organisms/header/header.php';
+          include './front/components/molecules/buttons/navbutton/navbutton.php';
+              
+          navbuttonComponent('#','button');
+          // buttonComponent('Click me!');
+        ?>
+
+    </header>
+  </body>
+</html>
+
--- index.php
+++ index.php
@@ -5,30 +5,27 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <!-- <link rel="stylesheet" href="./front/components/organisms/header/header.css"> -->
-    
-    <link rel="stylesheet" href="./front/style/reset.css">
-    
-    <link rel="stylesheet" href="./front/style/colors.css">
-    <link rel="stylesheet" href="./front/style/text-styles.css">
-    <link rel="stylesheet" href="./front/style/main.css">
-
-    <link rel="stylesheet" href="./front/components/molecules/buttons/button.css">
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title> Rugby World Cup 2023 by ESIN</title>
-</head>
-<body>
-    <?php
-        // include './front/components/organisms/header/header.php';
-        include './front/components/molecules/buttons/navbutton.php';
+    <head>
         
-        navbuttonComponent('#','navbutton');
-    ?>
+        <!-- style.css -->
+        <link rel="stylesheet" href="./front/style/colors.css">
+        <link rel="stylesheet" href="./front/style/text-styles.css">
+        <!-- main.css -->
+        <link rel="stylesheet" href="./front/style/main.css">
+        <!-- COMPONENTS -->
+        <!-- Header -->
+        <!-- <link rel="stylesheet" href="./front/components/organisms/header/header.css"> -->
 
-</body>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title> Rugby World Cup 2023 by ESIN</title>
+    </head>
+    <body>
+        <?php
+                
+        ?>  
+
+    </body>
 </html>
