11977b05ff4305d4ad792553c5968f59f5800169
Bel-kun <bellaire.vivien22@gmail.com>
2023-06-25 19:29:07 -0400

    Add meetings components

--- front/components/organisms/meetings/meetings.php
+++ front/components/organisms/meetings/meetings.php
@@ -1,26 +1,16 @@
-<?php
-    include './bdd/meetings/Meetings.php';
-?>
-
 <section class="o-meetings">
     <h2 class="o-meetings__title title neutral100">SCORE DES RENCONTRES</h2>
 
     <div class="o-meetings__table">
         <?php
+            include './bdd/meetings/Meetings.php';
 
             $meeting = new Meetings();
             $meetings = $meeting->findAll();
 
             foreach($meetings as $a_meeting){
-                ?>
-                    <p><?=$a_meeting['datetime']?><p>
-                    <p><?=$a_meeting['team_1']?><p>
-                    <p><?=$a_meeting[ 'team_2']?><p>
-                    <br>
-                    <p><?=$a_meeting['place']?><p>
-                    <br>
-                    <p><?=$a_meeting['score']?><p>
-                <?php
+
+                include './front/components/molecules/meeting/meeting.php';
             }
         ?>
     </div>
