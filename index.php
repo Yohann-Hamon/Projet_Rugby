55a9b9ae088d72af82fc0b4f3c250f3be63a1793
Bel-kun <bellaire.vivien22@gmail.com>
2023-06-25 03:25:50 -0400

    Fix design tokens and add reset.css

--- index.php
+++ index.php
@@ -6,14 +6,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- <link rel="stylesheet" href="./front/components/organisms/header/header.css"> -->
+    
+    <link rel="stylesheet" href="./front/style/reset.css">
+    
+    <link rel="stylesheet" href="./front/style/colors.css">
+    <link rel="stylesheet" href="./front/style/text-styles.css">
+    <link rel="stylesheet" href="./front/style/main.css">
+
+    <link rel="stylesheet" href="./front/components/molecules/buttons/button.css">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet Rugby</title>
+    
+    <title> Rugby World Cup 2023 by ESIN</title>
 </head>
 <body>
-
-
+    <?php
+        // include './front/components/organisms/header/header.php';
+        include './front/components/molecules/buttons/navbutton.php';
+        
+        navbuttonComponent('#','navbutton');
+    ?>
 
 </body>
 </html>
