f802e83cf88cd3c684c838bfd79e12a30f71c048
unknown <hassan.iqbal1@outlook.fr>
2023-06-26 22:48:08 +0200

    Affichage equipe

--- front/components/molecules/team/team.php
+++ front/components/molecules/team/team.php
@@ -1,5 +1,21 @@
-<div class="o-team">
-    <img class="o-team__emblem" src="<?=$a_team['emblem']?>" alt="emblem">
-    <p><?=$a_team['world_ranking']?><p>
-    <p><?=$a_team['country']?><p>
-</div>
+<table>
+  <tr>
+    <td><img class="o-team__emblem" src="<?=$a_team['emblem']?>" alt="emblem"></td>
+    <td><?=$a_team['world_ranking']?></td>
+    <td><?=$a_team['country']?></td>
+  </tr>
+</table>
+
+<style>
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+  
+  td {
+    border: 1px solid black;
+    padding: 8px;
+    text-align: center;
+  }
+</style>
+
