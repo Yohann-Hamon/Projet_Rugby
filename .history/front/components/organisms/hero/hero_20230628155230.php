4c88c1cd3f48e3e9696595aeb5979b8e318408b3
inessben <ine.benss@gmail.com>
2023-06-28 16:02:50 +0200

    delete text style

---
+++ .history/front/components/organisms/hero/hero_20230628155230.php
@@ -0,0 +1,5 @@
+<section class="o-hero">
+    <h2 class="o-hero__title  title  neutral400">A partir du 8 septembre 2023</h2>
+    <h1 class="o-hero__main-title  main-title  neutral100">Venez suivre le XV d’oeuFrance*</h1>
+    <h3 class="o-hero__main-title  main-title  neutral100">Le plus prestigieux tournoi de rugby au monde</h1>
+</section>
