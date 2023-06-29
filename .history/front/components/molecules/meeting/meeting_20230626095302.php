313f2be0c23ae5b4560a1c56cba0748d327c3866
inessben <ine.benss@gmail.com>
2023-06-29 12:04:13 +0200

    meeting page

---
+++ .history/front/components/molecules/meeting/meeting_20230626095302.php
@@ -0,0 +1,9 @@
+<div class="o-meeting">
+    <p><?=$a_meeting['datetime']?><p>
+    <div class="o-meeting__competitors">
+        <p><?=$a_meeting['team_1']?><p>
+        <p><?=$a_meeting['score']?><p>
+        <p><?=$a_meeting['team_2']?><p>
+    </div>
+    <p><?=$a_meeting['place']?><p>
+</div>
