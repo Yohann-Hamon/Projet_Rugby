b7aba210bfe437fc4481aa7bb4c81da6322e5c4d
inessben <ine.benss@gmail.com>
2023-06-29 12:43:52 +0200

    meeting page

--- .history/front/components/organisms/meetings/meetings_20230628231128.php
+++ .history/front/components/organisms/meetings/meetings_20230628231128.php
@@ -1,5 +1,5 @@
 <section class="o-meetings">
-    <h2 class="o-meetings title primary900">Les rencontres</h2>
+    <h2 class="o-meetings__title title neutral100">SCORE DES RENCONTRES</h2>
 
     <div class="o-meetings__table">
         <?php
