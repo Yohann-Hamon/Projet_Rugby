7c4bb490bcc42524bf6db8a3b165906624833009
Bel-kun <bellaire.vivien22@gmail.com>
2023-06-30 08:45:23 -0400

    delette player and teams

--- front/components/molecules/team/team.php
+++
@@ -1,9 +0,0 @@
-<table class="o-team js-team">
-  <tr>
-    <td><?=$a_team['world_rank']?></td>
-    <td><img class="o-team__emblem" src="<?=$a_team['emblem']?>" alt="emblem"></td>
-    <td><?=$a_team['country']?></td>
-    <td><?=$a_team['pool']?></td>
-  </tr>
-</table>
-
